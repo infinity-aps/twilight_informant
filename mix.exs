4ec4ccf05ac6d785bae90210045323346644946e
Tim Mecklem <timothy@mecklem.com>
2017-10-22 17:09:56 -0400

    Update mix.exs for hex package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule TwilightInformant.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     name: "Twilight Informant",
+     source_url: "https://github.com/infinity-aps/twilight_informant",
+     description: description(),
+     package: package()]
   end
 
   def application do
@@ -18,5 +22,19 @@ defmodule TwilightInformant.Mixfile do
     [{:httpoison, "~> 0.12"},
      {:poison, "~> 3.1"},
      {:timex, "~> 3.0"}]
+  end
+
+  defp description do
+    """
+    Twilight Informant is an Elixir client for the Nightscout (cgm-remote-monitor) JSON API.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Timothy Mecklem"],
+      licenses: ["MIT License"],
+      links: %{"Github" => "https://github.com/infinity-aps/twilight_informant"}
+    ]
   end
 end
